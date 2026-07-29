fd9a77980546e9dc430db9da98ced09664e80a61
Adeoye100 <adeoyeopeyemi951@gmail.com>
2026-07-29 12:09:20 +0100

    front and backend connection - add CORS, unify API fetch

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const app = express();
 app.use(helmet());
 app.use(
   cors({
-    origin: ["http://localhost:3000", "null"],
+    origin: process.env.FRONTEND_ORIGIN || "http://localhost:3000",
     credentials: true,
   })
 );
