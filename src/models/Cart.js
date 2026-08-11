635a03d6bdea4d9ab5f36d03b2904d0c02df10fc
Adeoye100 <adeoyeopeyemi951@gmail.com>
2026-08-11 10:34:00 +0100

    fix(cart): persist price-at-add snapshot

--- src/models/Cart.js
+++ src/models/Cart.js
@@ -1,36 +1,18 @@
 import mongoose from "mongoose";
-import Variant from "./Variant.js";
 
 const { Schema, model } = mongoose;
 
 const CartSchema = new Schema({
   userId: { type: Schema.Types.ObjectId, ref: 'User', index: true },
-  guestId: { type: String, index: true },
   items: [
     {
       productId: { type: Schema.Types.ObjectId, ref: 'Product', required: true },
       variantSku: { type: String, required: true },
       quantity: { type: Number, required: true, min: 1 },
+      priceAtAdd: { type: Number, required: true },
     },
   ],
 }, { timestamps: true });
-
-CartSchema.statics.mergeGuestCartIntoUser = async function(guestId, userId) {
-  const [guestCart, userCart] = await Promise.all([
-    this.findOne({ guestId }),
-    this.findOneAndUpdate({ userId }, {}, { upsert: true, new: true }),
-  ]);
-  if (!guestCart) return userCart;
-
-  for (const gItem of guestCart.items) {
-    const existing = userCart.items.find((i) => i.variantSku === gItem.variantSku);
-    if (existing) existing.quantity += gItem.quantity;
-    else userCart.items.push(gItem);
-  }
-  await userCart.save();
-  await guestCart.deleteOne();
-  return userCart;
-};
 
 const Cart = model('Cart', CartSchema);
 
